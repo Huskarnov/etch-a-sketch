6648eae9f572bdf66ade602109f683d4c616daf9
Huskarnov <youcef.merad.batna2m@gmail.com>
2024-09-18 19:51:47 +0100

    done

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let gridSize;
+let elementSize = prompt("chose a size");
 
 let theGrid = document.querySelector('.the-grid');
 
@@ -8,11 +9,22 @@ let sizeButton = document.querySelector('button');
 
 let arrayOfDivs =[];
 
-for(let i = 0; i < 64; i++){
-    arrayOfDivs.push(standardDiv.cloneNode());
-}
 
-arrayOfDivs.forEach(element => {
+function cloneAndAdd(side){
+    arrayOfDivs = [];
+for(let i = 0; i < side*side; i++){
+    arrayOfDivs.push(standardDiv.cloneNode());
+    console.log(arrayOfDivs.i);
+    }
+}
+cloneAndAdd(elementSize);
+
+function styleAndAppend(dimention){
+    arrayOfDivs.forEach(element => {
+    let sHeight = (550 / dimention) - 1;
+    element.style.height = `${sHeight}px`;
+    element.style.width = `${sHeight}px`;
+
     element.addEventListener("mouseover", function(){
 
         element.style.backgroundColor = "orange";
@@ -23,9 +35,16 @@ arrayOfDivs.forEach(element => {
         )
     }
 );
-for (let y = 0; y < arrayOfDivs.length; y++){
-    theGrid.appendChild(arrayOfDivs[y]);
 }
+styleAndAppend(elementSize);
+
+function appendAndWrapThisShit(){
+    for (let y = 0; y < arrayOfDivs.length; y++){
+    theGrid.appendChild(arrayOfDivs[y]);
+    }
+}
+appendAndWrapThisShit();
+
 
 sizeButton.addEventListener('click', function(){
     gridSize = prompt('Chose a grid size');
@@ -33,18 +52,14 @@ sizeButton.addEventListener('click', function(){
     if (!isNaN(gridSize)){
         while(theGrid.lastChild){
             theGrid.removeChild(theGrid.lastChild);
-            
+
             }
         
-        // for(let i = 0; i < gridSize; i++){
-        //     arrayOfDivs.push(standardDiv.cloneNode());
-            
-        // }
+        cloneAndAdd(gridSize);
 
-        // for (let y = 0; y < arrayOfDivs.length; y++){
-        //     theGrid.appendChild(arrayOfDivs[y]);
-        // }
+        styleAndAppend(gridSize);
 
+        appendAndWrapThisShit();
         
     }else{
         alert('Nigga that aint a numba !')
